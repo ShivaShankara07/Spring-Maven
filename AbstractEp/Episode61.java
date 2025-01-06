7576ebb41cfb8bdbf472c9d42cf922722e4baa87
shivabollam07 <shivabollam07@gmail.com>
2025-01-06 10:12:22 +0530

    abstarct file add

--- AbstractEp/Episode61.java
+++ AbstractEp/Episode61.java
@@ -31,3 +31,11 @@ public class Episode61 {
         obj1.language();
     }
 }
+
+/** Notes:
+    Abstarct method should be in Abstarct class
+    we can have normal methods in Abstarct class
+    we cant create objects of abstarct class
+    we must and should implement the abstract methods in child class unless the child class is also abstarct class
+    if we dont want to create abstract method from the abstract parent class then we have to make the child class abstarct (point to be noted now we cant create an object of child class because it is abstarct class now)
+**/
